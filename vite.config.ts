84369e643d90abdf0e0f4124faf99f646361cd20
Prateek Giri <prateekgiri08@gmail.com>
2025-08-22 01:28:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/MOOD-MUSIC/", // 👈 repo name goes here
+  base: '/',  
 });
